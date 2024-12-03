9f8d410769ad90afcbf565f4c20bfbd4856be8ad
ART89-prog <72185684+ART89-prog@users.noreply.github.com>
2024-12-03 16:43:37 +0300

    484687756876

--- js/scripts.js
+++ js/scripts.js
@@ -42,41 +42,5 @@ $(() => {
   })
 
 
-  	// Моб. меню
-	$('header .mob_menu_btn').click((e) => {
-		e.preventDefault()
-
-		$('header .mob_menu_btn').addClass('active')
-		$('body').addClass('menu_open')
-		$('header .menu').addClass('show')
-		$('.overlay').fadeIn(300)
-	})
-
-	$('header .close_btn, header .menu .item a, .overlay').click((e) => {
-		e.preventDefault()
-
-		$('header .mob_menu_btn').removeClass('active')
-		$('body').removeClass('menu_open')
-		$('header .menu').removeClass('show')
-		$('.overlay').fadeOut(300)
-	})
-
-
-
-  // Скрол
-  $(".scroll").on("click", function (e) {
-    e.preventDefault();
-    let id = $(this).attr("href");
-
-    $("html, body").animate({
-      scrollTop: $(id).offset().top + "px"
-    }, {
-      duration: 1500,
-      easing: "swing"
-    });
-  });
-
-
-
 
 })
